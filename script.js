2f3c9ff790b1e741896b1ac47101a861c22661a0
Giancarlo Rios <giantherios11@gmail.com>
2023-09-14 21:21:12 -0400

    new file:   index.html new file:   script.js new file:   style.css

---
+++ script.js
@@ -0,0 +1,28 @@
+const switchBtn = document.getElementById('switch');
+const handle = document.getElementById('handle');
+
+let switchStatus = 0; // 0 = off, 1 = on
+
+switchBtn.addEventListener('click', toggleMode);
+
+function toggleMode() {
+  // light mode on
+  if (switchStatus === 0) {
+    document.documentElement.style.setProperty('--background-color', 'white');
+    document.documentElement.style.setProperty('--text-color', 'black');
+    handle.style.transform = 'translateX(45px)';
+    switchStatus = 1;
+  } else {
+    // dark mode on
+    switchStatus = 0;
+    document.documentElement.style.setProperty('--background-color', '#232323');
+    document.documentElement.style.setProperty('--text-color', 'white');
+    handle.style.transform = 'translateX(0)';
+
+  }
+
+}
+
+
+
+
